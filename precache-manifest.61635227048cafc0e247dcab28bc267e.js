1503371ca8759f1c556173f9cb12937c9465a1a8
Vladimir Ventura <vladventura@outlook.es>
2020-03-27 20:58:06 -0400

    Updates

---
+++ precache-manifest.61635227048cafc0e247dcab28bc267e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f86a07a8b6b0b3754b32cc4a334e49",
+    "url": "/poketrivia/index.html"
+  },
+  {
+    "revision": "68149a3531dbedbd4c65",
+    "url": "/poketrivia/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "aa3ee1938b3e7babf698",
+    "url": "/poketrivia/static/css/main.304c34db.chunk.css"
+  },
+  {
+    "revision": "68149a3531dbedbd4c65",
+    "url": "/poketrivia/static/js/2.f6c28717.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/poketrivia/static/js/2.f6c28717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3ee1938b3e7babf698",
+    "url": "/poketrivia/static/js/main.d73a6d64.chunk.js"
+  },
+  {
+    "revision": "52e727d767e48f32697c",
+    "url": "/poketrivia/static/js/runtime-main.3dc61d40.js"
+  }
+]);
